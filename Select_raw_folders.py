cd0677a3ef15c1c6bbab1daa5bae07d9897676da
Ian Reah <ian_reah@waters.com>
2022-12-15 15:16:01 +0000

    Replace aggrid with a list of checkboxes

--- Select_raw_folders.py
+++ Select_raw_folders.py
@@ -1,11 +1,8 @@
 import os
 import glob
 
-import pandas as pd
 import dotenv
-
 import streamlit as st
-from st_aggrid import AgGrid, GridOptionsBuilder, DataReturnMode, GridUpdateMode
 
 from Waters2mzML import get_config, process_all_raw, ms_convert, annotate
 
@@ -14,6 +11,23 @@ def _set_default_config(default_value, env_key):
     if os.getenv(env_key) is None:
         os.environ[env_key] = default_value
         dotenv.set_key(dotenv_file, env_key, os.environ[env_key])
+
+
+def _get_raw_checkbox_state(raw, select_all, select_none):
+    if select_all:
+        return True
+
+    if select_none:
+        return False
+
+    if f'raw_checkbox_{raw}' in st.session_state:
+        return st.session_state[f'raw_checkbox_{raw}']
+
+    return False
+
+
+def _get_selected_raws():
+    return [x.replace('raw_checkbox_', '') for x in st.session_state.keys() if x.startswith('raw_checkbox_') and st.session_state[x]]
 
 
 st.set_page_config(layout="wide")
@@ -43,26 +57,27 @@ st.title('Waters2mzML')
 
 if os.getenv('MSCONVERT_EXE') is None:
     st.error('Missing `msconvert.exe`. Use the Setup page on the left to browse to this file.')
+    st.stop()
 
 st.markdown(f'''Copy your Waters .raw folders to `{os.getenv('RAW_FILES_FOLDER')}` for conversion.  
 mzML files will be saved to `{os.getenv('MZML_FILES_FOLDER')}`.''')  
 st.caption('Use the Setup page on the left to configure these locations')
 
 st.subheader('Select .raw folders to convert')
-st.button('Refresh the list')
-all_raw = pd.DataFrame(glob.glob('*.raw', root_dir=os.getenv('RAW_FILES_FOLDER')), columns=["RAW Folder"])
-grid_builder = GridOptionsBuilder.from_dataframe(all_raw)
-grid_builder.configure_default_column(filterable=False, sorteable=False)
-grid_builder.configure_selection(selection_mode='multiple', use_checkbox=True)
-raw_grid = AgGrid(
-    data=all_raw, editable=False, gridOptions=grid_builder.build(), data_return_mode=DataReturnMode.FILTERED,
-    height=200, update_mode=GridUpdateMode.SELECTION_CHANGED, theme="streamlit"
-)
+columns = st.columns(8)
+columns[0].button('Refresh the list')
+select_all = columns[1].button('Select All')
+select_none = columns[2].button('Select None')
 
-centroid = st.checkbox('Centroid profiled data?')
-process = st.button('Start conversion')
+all_raw = glob.glob('*.raw', root_dir=os.getenv('RAW_FILES_FOLDER'))
+for raw in all_raw:
+    st.checkbox(raw, key=f'raw_checkbox_{raw}', value=_get_raw_checkbox_state(raw, select_all, select_none))
+
+centroid = st.sidebar.checkbox('Centroid profiled data?')
+_, right = st.sidebar.columns(2)
+process = right.button('Start conversion')
 if process:
-    raws_to_process = [os.path.join(os.getenv('RAW_FILES_FOLDER'), x['RAW Folder']) for x in raw_grid['selected_rows']]
+    raws_to_process = [os.path.join(os.getenv('RAW_FILES_FOLDER'), x) for x in _get_selected_raws()]
     ms2 = process_all_raw(os.getenv('RAW_FILES_FOLDER'), raws_to_process)
     ms_convert(os.getenv('RAW_FILES_FOLDER'), os.getenv('MSCONVERT_EXE'), raws_to_process, get_config(centroid))
     annotate(os.getenv('RAW_FILES_FOLDER'), os.getenv('MZML_FILES_FOLDER'), ms2)
